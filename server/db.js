3efb9cb22784e7984306eb69e924b9c08839ddca
EC2 Default User <ec2-user@ip-172-31-47-163.us-east-2.compute.internal>
2024-02-21 17:24:42 +0000

    passwords hashed

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_store_db');
 const uuid = require('uuid');
+const bcrypt = require('bcrypt');
 
 const createTables = async()=> {
     const SQL = `
@@ -15,8 +16,7 @@ const createTables = async()=> {
     );
       CREATE TABLE products(
     id UUID PRIMARY KEY,
-    name VARCHAR(100) UNIQUE NOT NULL,
-   
+    name VARCHAR(100) UNIQUE NOT NULL
     );
     CREATE TABLE user_favorites(
     id UUID PRIMARY KEY,
@@ -25,11 +25,53 @@ const createTables = async()=> {
     CONSTRAINT unique_product_user UNIQUE (product_id, user_id)
 );
     `;
+    await client.query(SQL);
 };
+    const createUser = async({ username, password })=> {
+  const SQL = `
+    INSERT INTO users( id, username, password ) 
+    VALUES($1, $2, $3) 
+    RETURNING *
+  `;
+  const response = await client.query(SQL, [uuid.v4(), username, await bcrypt.hash(password, 5)]);
+  return response.rows[0];
+};
+
+const createProduct = async({ name })=> {
+  const SQL = `
+    INSERT INTO products(id, name) 
+    VALUES($1, $2) 
+    RETURNING *
+  `;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+
+const fetchUsers = async()=> {
+  const SQL = `
+    SELECT * FROM users;
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const fetchProducts = async()=> {
+  const SQL = `
+    SELECT * FROM products;
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
 
 
 
 module.exports = {
     client,
     createTables,
+    createUser,
+   createProduct,
+   fetchUsers,
+   fetchProducts
+   
 };
--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 const { 
     client, 
     createTables,
+    createUser,
+    createProduct
 } = require('./db');
 
 const init = async()=> {
   await client.connect();
+  console.log('connecting to database');
   console.log('connected to database');
   await createTables();
   console.log('tables created');
+    const [jack, sally, bob, laptop, iphone, ipad, backpack] = await Promise.all([
+    createUser({ username: 'jack', password: 'hello' }),
+    createUser({ username: 'sally', password: 'hi' }),
+    createUser({ username: 'bob', password: 'shhh' }),
+     createProduct({ name: 'laptop'}),
+    createProduct({ name: 'iphone'}),
+    createProduct({ name: 'ipad'}),
+    createProduct({ name: 'backpack'}),
+    ]);
+    console.log(jack.id);
+    console.log(laptop.id);
 };
 
 init();
